fe211daf1d4cce65e6410db4bf9c722b227d4e02
Adrian-Xavier <adrxavmiranda2001@gmail.com>
2024-01-29 13:44:26 +0800

    Added the Student directories
    Changed all the icons and navigation and stuff

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/reset',function(){
     return view('reset_password');
 });
+
+
 
 Route::get('/',function(){
     return view('login');
 });
 
 Route::post('/submitcode', [ForgotPassword::class,'submit']);
+
+
+//Routes for students
+Route::get('/student/home',function(){
+    return view('student.home');
+});
+
+Route::get('/student/announcement',function(){
+    return view('student.announcement');
+});
+Route::get('/student/calendar',function(){
+    return view('student.calendar');
+});
+Route::get('/student/time_record',function(){
+    return view('student.time_record');
+});
+Route::get('/student/user',function(){
+    return view('student.user');
+});
 
 
 //Routes for admins
