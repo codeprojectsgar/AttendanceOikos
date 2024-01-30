0c87f5df03b200c97094dfc065248a614945e8c3
KafelnikovDelaRosa <kafelnikovdelarosa@yahoo.com>
2024-01-30 11:49:00 +0800

    Implemented front-end validation  for resetting password

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -33,50 +33,7 @@
     </div>
 
     <!-- Optional JavaScript -->
-    <script>
-        let email_field=document.getElementById('EI');
-        let pass_field=document.getElementById('PI');
-        let error=document.getElementById('user-error');
-        let csrf=document.querySelector("meta[name='csrf-token']")
-        function submitForm(email_data,pass_data){
-            fetch('/login-user', {
-                method:'POST', 
-                headers:{'Content-Type':'application/json', 'X-CSRF-Token': csrf.content},
-                body: JSON.stringify({'email': email_data, 'password': pass_data})
-            })
-            .then(response => response.json())
-            .then(data => {
-                if(data.success && data.role===1){
-                    window.location.href='/admin/dashboard'
-                }
-                else{
-                    error.textContent="Incorrect Credentials!";
-                }
-            })
-            .catch(error => {
-                console.error('Error Loging In', error)
-            })
-        } 
-        //This is the handleForm button event when clicked
-        function handleForm(){
-            let email = email_field.value;
-            let password = pass_field.value;
-            let email_err_field=document.getElementById('email-error')
-            let pass_err_field=document.getElementById('password-error');
-            if(password.length===0){
-                pass_err_field.textContent="Password is required!";
-            }
-            if(email.length===0){
-                email_err_field.textContent="Email is required!";
-            }
-            else{
-                pass_err_field.textContent="";
-                email_err_field.textContent="";
-                submitForm(email,password);
-            }
-        }
-    </script>
-
+    <script src="/JS/login.js"></script>
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
