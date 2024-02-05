b38c0c263c9dc40a7b05c1be63c7e7f830b377f8
KafelnikovDelaRosa <kafelnikovdelarosa@yahoo.com>
2024-02-05 10:40:23 +0800

    Implemented component file for easier maintainability and scalability

--- resources/views/student/calendar.blade.php
+++ resources/views/student/calendar.blade.php
@@ -14,71 +14,10 @@
     <title>Oikos Student: Calendar</title>
 </head>
 <body>
-    <div class="sidebar">
-        <div class="top">
-            <div class="logo">
-                <i class="fa-solid fa-user-secret"></i>
-                <span>Oikos</span>
-            </div>
-            <i class="fa-solid fa-bars" id = "btn"></i>
-        </div>
-        <div class="user">
-            <img src ="" alt="secret-user" class = "user-img">
-            <div class="">
-                <p class = "bold">Kafelnikov Dela Rosa</p>
-                <p>(Student)</p>
-            </div>
-        </div>
-        <ul>
-            <li>
-                <a href = "/students/Home">
-                    <i class="fa-solid fa-house"></i>
-                    <span class="nav-item">Home</span>
-                </a>
-                <span class="tooltip">Home</span>
-            </li>
-            <li>
-                <a href = "/students/User-Info">
-                    <i class="fa-solid fa-user"></i>
-                    <span class="nav-item">User</span>
-                </a>
-                <span class="tooltip">User</span>
-            </li>
-            <li>
-                <a href = "/students/Announcement">
-                    <i class="fa-solid fa-bullhorn"></i>
-                    <span class="nav-item">Announcement</span>
-                </a>
-                <span class="tooltip">Announcement</span>
-            </li>
-            <li>
-                <a href = "/students/Calendar">
-                    <i class="fa-solid fa-calendar"></i>
-                    <span class="nav-item">Calendar</span>
-                </a>
-                <span class="tooltip">Calendar</span>
-            </li>
-            <li>
-                <a href = "/students/Time-Logs">
-                    <i class="fa-solid fa-clock"></i>
-                    <span class="nav-item" style="height:50px;">Time Record</span>
-                </a>
-                <span class="tooltip">Time Record</span>
-            </li>
-            
-            <li>
-                <a href = "/studentlogout">
-                    <i class="fa-solid fa-right-from-bracket"></i>
-                    <span class="nav-item">Logout</span>
-                </a>
-                <span class="tooltip">Logout</span>
-            </li>
-        </ul>
-    </div>
-     <div class="main-content">
+    @include('component.student.sidenav')
+    <div class="main-content">
         <div class="Calendar1">
             <h1 id = "CalendarHeader" >Calendar</h1>
-                 
                 <div class="DateClickedBG"></div>
                 <button onclick="openAddNotesPopup()" class="button notesb">+ Add Notes</button>
                 <div class="DateClicked">
--- resources/views/student/home.blade.php
+++ resources/views/student/home.blade.php
@@ -161,70 +161,9 @@
 
     
 </style>
-   
 </head>
 <body>
-    <div class="sidebar">
-        <div class="top">
-            <div class="logo">
-                <i class="fa-solid fa-user-secret"></i>
-                <span>Oikos</span>
-            </div>
-            <i class="fa-solid fa-bars" id = "btn"></i>
-        </div>
-        <div class="user">
-            <img src ="" alt="secret-user" class = "user-img">
-            <div class="">
-                <p class = "bold">Kafelnikov Dela Rosa</p>
-                <p>(Student)</p>
-            </div>
-        </div>
-        <ul>
-            <li>
-                <a href = "/students/Home">
-                    <i class="fa-solid fa-house"></i>
-                    <span class="nav-item">Home</span>
-                </a>
-                <span class="tooltip">Home</span>
-            </li>
-            <li>
-                <a href = "/students/User-Info">
-                    <i class="fa-solid fa-user"></i>
-                    <span class="nav-item">User</span>
-                </a>
-                <span class="tooltip">User</span>
-            </li>
-            <li>
-                <a href = "/students/Announcement">
-                    <i class="fa-solid fa-bullhorn"></i>
-                    <span class="nav-item">Announcement</span>
-                </a>
-                <span class="tooltip">Announcement</span>
-            </li>
-            <li>
-                <a href = "/students/Calendar">
-                    <i class="fa-solid fa-calendar"></i>
-                    <span class="nav-item">Calendar</span>
-                </a>
-                <span class="tooltip">Calendar</span>
-            </li>
-            <li>
-                <a href = "/students/Time-Logs">
-                    <i class="fa-solid fa-clock"></i>
-                    <span class="nav-item" style="height:50px;">Time Record</span>
-                </a>
-                <span class="tooltip">Time Record</span>
-            </li>
-            
-            <li>
-                <a onclick="logout()">
-                    <i class="fa-solid fa-right-from-bracket"></i>
-                    <span class="nav-item">Logout</span>
-                </a>
-            </li>
-        </ul>
-    </div>
-
+    @include('component.student.sidenav')
     <div class="main-content">
         <h1 style="float:left; font-size:25px; margin:1rem;  letter-spacing:5px;" >HOME</h1>
         <div class="container">
