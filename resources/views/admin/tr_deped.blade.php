c9bd9df834d1ac9cfd793658bba46b0598204d0f
KafelnikovDelaRosa <kafelnikovdelarosa@yahoo.com>
2024-02-07 13:18:58 +0800

    Accomplished Time Record for DepEd

--- resources/views/admin/tr_deped.blade.php
+++ resources/views/admin/tr_deped.blade.php
@@ -10,14 +10,150 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <title>Oikos Admin: DepEd Form</title>
+    <style>
+        h1 a{
+            text-decoration:none;
+            color:gray;
+            transition:color 150ms ease-in-out;
+        }
+        h1 a:hover{
+            color:black;
+        }
+        .lists{
+            background-color:white;
+            padding:1em;
+            border-radius:5px;
+        }
+        .lists .view{
+            display:flex;
+            justify-content: space-between;
+            align-items:center;
+            border-bottom:2px solid #c5c5c5;
+            margin-top:.5em;
+        }
+        .view .label-group{
+            width:35%;
+        } 
+        .view .label-group p{
+            font-size:1.2rem;
+        }
+        .view .dropdown i{
+            margin-right:.6rem;
+            cursor:pointer;
+            transform:rotate(0deg);
+            transition:transform 150ms ease-in-out;
+        }
+        .view .dropdown .animate{
+            transform:rotate(90deg);
+        }
+        .lists .detail{
+            visibility:hidden;
+            position:absolute;
+            display:flex;
+            justify-content:space-between;
+            color:white;
+            padding:.5em;
+            background-color:#51558f;
+        }
+        .lists .detail .option p{
+            font-size:1.1rem;
+        }
+        .lists .detail .option-choice{
+            display:flex;
+            justify-content: space-around;    
+        }
+        .detail.show{
+            visibility: visible;
+            position:relative;
+        }
+    </style>
 </head>
 <body>
     @include('component.admin.sidenav')
-   
 
     <div class="main-content">
-        <h1>Time Record</h1>
+        <h1><a href="/admin/Time_Record">Time Record > </a> DepEd</h1>
         <div class="container">
+            <div class="lists">
+                <!--Create a drop down-->
+                <div class="view">
+                    <div class="label label-group">
+                        <p>Dep Ed Form</p>
+                        <p>January 1, 2024</p>
+                    </div>
+                    <div class="label-grade label-group">
+                        <p>Grade:1</p>
+                    </div>
+                    <div class="dropdown">
+                        <i class="fas fa-chevron-right _1" onclick="dropInfo(1)" aria-expanded="false"></i>
+                    </div>
+                </div>
+                <div id="_1" class="detail">
+                    <div class="option">
+                        <p>Section 1: Alpha</p>
+                        <div class="option-choice">
+                            <i class="fas fa-print"></i>
+                            <i class="fas fa-file-export"></i>
+                        </div>
+                    </div>
+                    <div class="option">
+                        <p>Section 2: Beta</p>
+                        <div class="option-choice">
+                            <i class="fas fa-print"></i>
+                            <i class="fas fa-file-export"></i>
+                        </div>
+                    </div>
+                    <div class="option-all">
+                        <div class="print-all">
+                            <i class="fas fa-print"></i>
+                            <small>Print all</small>
+                        </div>
+                        <div class="export-all">
+                            <i class="fas fa-file-export"></i>
+                            <small>Export all</small>
+                        </div>
+                    </div>
+                </div>
+                <!-- refer to the code above for drop down-->
+                <div class="view">
+                    <div class="label label-group">
+                        <p>Dep Ed Form</p>
+                        <p>January 1, 2024</p>
+                    </div>
+                    <div class="label-grade label-group">
+                        <p>Grade:2</p>
+                    </div>
+                    <div class="dropdown">
+                        <i class="fas fa-chevron-right _2" onclick="dropInfo(2)" aria-expanded="false"></i>
+                    </div>
+                </div>
+                <div id="_2" class="detail">
+                    <div class="option">
+                        <p>Section 1: Honesty</p>
+                        <div class="option-choice">
+                            <i class="fas fa-print"></i>
+                            <i class="fas fa-file-export"></i>
+                        </div>
+                    </div>
+                    <div class="option">
+                        <p>Section 2: Fidelity</p>
+                        <div class="option-choice">
+                            <i class="fas fa-print"></i>
+                            <i class="fas fa-file-export"></i>
+                        </div>
+                    </div>
+                    <div class="option-all">
+                        <div class="print-all">
+                            <i class="fas fa-print"></i>
+                            <small>Print all</small>
+                        </div>
+                        <div class="export-all">
+                            <i class="fas fa-file-export"></i>
+                            <small>Export all</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -25,7 +161,6 @@
     <script>
         let btn = document.querySelector('#btn');
         let sidebar = document.querySelector('.sidebar');
-
         btn.onclick = function () {
             sidebar.classList.toggle('active');
         }
@@ -43,6 +178,23 @@
                     window.location.href='/admin/logout';
                 }
             });
+        }
+        function dropInfo(id){
+            let classSelector="._"+id;
+            let idSelector="_"+id;
+            let dropdown=document.querySelector(classSelector);
+            let detailEl=document.getElementById(idSelector);
+            let isExpanded=dropdown.getAttribute("aria-expanded");
+            if(isExpanded==="false"){
+                dropdown.setAttribute('aria-expanded','true');
+                dropdown.classList.toggle('animate');
+                detailEl.classList.toggle('show');
+            }
+            else{
+                dropdown.setAttribute('aria-expanded','false');
+                dropdown.classList.remove('animate');
+                detailEl.classList.remove('show');
+            }
         }
 
     </script>
